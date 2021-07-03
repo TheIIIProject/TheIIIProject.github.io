2217eecf1f72ade053e713dc0e5d6f96089d4dee
InimicalPart <37552123+InimicalPart@users.noreply.github.com>
2021-07-04 00:30:47 +0200

    Make III website

---
+++ src/main.js
@@ -0,0 +1,7 @@
+function arrowPressed() {
+	document.getElementById("here").scrollIntoView()
+}
+function openTwtr() { window.open("https://www.twitter.com/theiiisociety") }
+function openIG() { window.open("https://www.instagram.com/the_iii_society/") }
+function openDC() { window.open("https://discord.gg/iii") }
+function openMail() { window.location.href = "mailto:theiiisociety.help@gmail.com?Subject=I%20need%20help%20with%3A" }
